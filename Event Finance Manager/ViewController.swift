7915fe65f8978099d37a7a5c6fe4534d67cf2d19
SaiKiran Panuganti <saikiranpanuganti92@gmail.com>
2022-04-14 21:16:33 +0530

    Initial Commit

---
+++ Event Finance Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Event Finance Manager
+//
+//  Created by Saikiran Panuganti on 14/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
